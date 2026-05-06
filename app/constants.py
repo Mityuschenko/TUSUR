f07ea020a125602aad2b6a2c0c9dee57178c387a
Максим Митющенко <maxmif1977@gmail.com>
2026-05-06 07:55:16 +0000

    Библиотеки + Работа с Google Диск + Настройка Git + Структура проекта

---
+++ app/constants.py
@@ -0,0 +1,10 @@
+
+CHROMA_PATH = "./store"
+
+CHROMA_COLLECTION = "embeddings"
+
+EMBEDDINGS_MODEL = "snowflake-arctic-embed2:latest"
+
+RECORD_MANAGER = "sqlite+aiosqlite:///record_manager_cache.sql"
+
+LLM_MODEL = "gemma3:4b"
